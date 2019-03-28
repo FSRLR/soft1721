0e1fdbcf275be1c58e8d54dc93472469a53729f3
ASUS <1723109431@qq.com>
2019-03-28 09:11:54 +0800

    soft1721

---
+++ src/main/java/com/example/soft1721/Soft1721Application.java
@@ -0,0 +1,13 @@
+package com.example.soft1721;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soft1721Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Soft1721Application.class, args);
+    }
+
+}
